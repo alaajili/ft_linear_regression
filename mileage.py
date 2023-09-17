934a43b00faf831854f420920f9cf796446d22e4
Abderrahmane Laajili <alaajili@c4r7p6.1337.ma>
2023-09-17 21:28:27 +0100

    finish work

--- mileage.py
+++ mileage.py
@@ -1,25 +1,38 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("data.csv");
-mileages = data['km'].tolist()
-prices = data['price'].tolist()
+_mileages = data['km'].tolist()
+_prices = data['price'].tolist()
+
+min_mileage = min(_mileages)
+max_mileage = max(_mileages)
+mileages = [(x - min_mileage) / (max_mileage - min_mileage) for x in _mileages]
+
+min_price = min(_prices)
+max_price = max(_prices)
+prices = [(x - min_price) / (max_price - min_price) for x in _prices]
 
 
 theta0 = 0
 theta1 = 0
-# predictions = [ (theta0 + (theta1 * x)) for x in mileages]
-# print(sum(predictions))
+
+# scaled_x = (x - min) / (max - min)
+# x- min = scaled_x * (max - min)
+# x  = scaled_x * (max - min) + min
 
 
-learning_rate = 0.0001
-epochs = 1000
+learning_rate = 0.1
+epochs = 500
 
 m = len(data)
-print(m)
 
 # hypothesis function
 def estimatePrice(mileage):
-    return theta0 + (theta1 * mileage)
+    scaled_mileage = (mileage - min_mileage) / (max_mileage - min_mileage)
+    scaled_price = (theta0 + (theta1 * scaled_mileage))
+    price = scaled_price * (max_price - min_price) + min_price
+    return price; 
 
 
 for epoch in range(epochs):
@@ -32,15 +45,21 @@ for epoch in range(epochs):
 
     # we calculate tmpTheta0 and tmpTheta1
     tmpTheta0 = learning_rate * (1/m) * sum(errors)
-    tmpTheta1 = learning_rate * (1/m) * sum([x * y for x, y in zip (errors, mileages)])
+    tmpTheta1 = learning_rate * (1/m) * sum([x * y for x, y in zip(errors, mileages)])
 
-    
-
-
-
+    theta0 -= tmpTheta0
+    theta1 -= tmpTheta1
 
 
+est_price = estimatePrice(22899)
+print(est_price)
 
-print(estimatePrice(240000))
+est_prices = [estimatePrice(mileage) for mileage in _mileages]
+
+
+plt.scatter(_mileages, _prices)
+plt.plot(_mileages, est_prices, color="red");
+
+plt.show()
 
 
